aa6a281300db0541617e555c776e5178506669a0
TymoWetselsPXL <12502212@student.pxl.be>
2025-11-06 10:45:58 +0100

    Eerste commit - C# oefening

---
+++ Oefening kijken of GITbash werkt/Oefening kijken of GITbash werkt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Oefening_kijken_of_GITbash_werkt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
